58098fceaf6390fbe08cc22288df587709efe478
ParedGuillermo <pared919@gmail.com>
2025-07-09 01:01:37 -0300

    Agregado modal en Productos para mostrar descripción completa; agegado numero de whatsapp

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -17,7 +17,7 @@ export default function CartModal({ onClose }) {
         .toFixed(2)}`
     : "Hola! No tengo productos en el carrito.";
 
-  const whatsappUrl = `https://wa.me/549113380557?text=${encodeURIComponent(
+  const whatsappUrl = `https://wa.me/5491133380557?text=${encodeURIComponent(
     whatsappMessage
   )}`;
 
