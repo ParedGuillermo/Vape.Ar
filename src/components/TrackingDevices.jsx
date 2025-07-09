7a5220cb5e3a2446831d80581376a23f1f1a6a2e
ParedGuillermo <pared919@gmail.com>
2025-07-09 00:25:12 -0300

    Agregar modal en Productos para mostrar descripción completa y mejora en listado sin descripción

--- src/components/TrackingDevices.jsx
+++ src/components/TrackingDevices.jsx
@@ -10,7 +10,7 @@ const TrackingDevices = () => (
         <h3 className="mb-2 font-semibold">Códigos QR de Rastreo</h3>
         <p className="text-sm text-gray-700">Ideal para mascotas. Fácil escaneo para rastrear en tiempo real.</p>
         <img
-          src="https://via.placeholder.com/150" // Imagen de referencia (reemplazar con imagen real)
+          src="https://zcoekpdxfbnooopsrrec.supabase.co/storage/v1/object/public/productos//dc60f746-49cc-41cb-8e86-e82a550b8f94.jpg" // Imagen de referencia (reemplazar con imagen real)
           alt="Código QR"
           className="object-cover w-32 h-32 mx-auto mt-4 rounded-full"
         />
@@ -32,7 +32,7 @@ const TrackingDevices = () => (
         <h3 className="mb-2 font-semibold">AirTags y Dispositivos Similares</h3>
         <p className="text-sm text-gray-700">Próximamente podrás rastrear a tu mascota con dispositivos como AirTag, Moto Tag, Galaxy Smart Tag, y más.</p>
         <img
-          src="https://via.placeholder.com/150" // Imagen de referencia (reemplazar con imagen real)
+          src="https://media.revistagq.com/photos/66bf16c66e782664741cc287/4:3/w_1440,h_1080,c_limit/I%20migliori%20smart%20tag%20per%20sapere%20sempre%20dove%20sono%20le%20tue%20valigie.jpg" // Imagen de referencia (reemplazar con imagen real)
           alt="AirTag"
           className="object-cover w-32 h-32 mx-auto mt-4 rounded-full"
         />
